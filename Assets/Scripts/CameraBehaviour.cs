ff99a7b37edbe86e7e2553db75ec98de52cb8677
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2023-05-04 17:28:49 +0200

    workaround for cam behind walls

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    private List<Renderer> hiddenRenderers;
+    [SerializeField] Transform player;
+
+    RaycastHit hit;
+    Ray ray;
+    Color rayColour;
+    Vector3 rayStartPosition;
+
+    void Start()
+    {
+        hiddenRenderers = new List<Renderer>();
+    }
+
+    void Update()
+    {
+        if (!GameManagement.guardsActive) return;
+        rayStartPosition = transform.position;
+        ray = new Ray(rayStartPosition, player.position - rayStartPosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.CompareTag("Wall"))
+            {
+                AddToList(hit.collider.gameObject.GetComponent<Renderer>());
+                rayColour = Color.green;
+            }
+            else
+            {
+                ClearList();
+                rayColour = Color.red;
+            }
+            Debug.DrawRay(rayStartPosition, player.position - rayStartPosition, rayColour);
+        }
+    }
+
+    private void AddToList(Renderer rend)
+    {
+        hiddenRenderers.Add(rend);
+        rend.enabled = false;
+    }
+
+    private void ClearList()
+    {
+        foreach (Renderer rend in hiddenRenderers) 
+        {
+            rend.enabled = true;
+        }
+        hiddenRenderers.Clear();
+    }
+}
